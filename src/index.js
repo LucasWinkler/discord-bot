642b6feecca9389c5c92ebd07286cebea96b3057
Lucas <lucasj.winkler1999@gmail.com>
2019-01-17 10:04:01 -0500

    Added basic functionality. ('!PING' command)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 // The apps config
-const config = require('../config.json')
+const config = require('../config.js')
 
+// The discord.js package and the client
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+// Listens for any messages sent in the server
+client.on('message', message => {
+    let sender = message.author; // The sender of the message
+    let msg = message.content.toUpperCase(); // The senders message
+
+    const prefix = '!'; // The character entered before a command
+
+    // Simple "Ping/Pong" command.
+    if (msg.toUpperCase() === prefix + 'PING') {
+        message.channel.send('Pong!');
+    }
+});
+
+// Connects the client using the auth token set in the config.js
+client.login(config.auth.token);

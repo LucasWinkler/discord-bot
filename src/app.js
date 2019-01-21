345e38b1e0aaf53a0191127a6402a5fe39ccc686
Lucas <lucasj.winkler1999@gmail.com>
2019-01-21 10:19:42 -0500

    Added message logging

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 // The apps config
 const config = require('../config')
 
-// The discord.js package and the client
+// The needed packages
 const Discord = require('discord.js');
+const moment = require('moment');
+
+// New instance of the discord client
 const client = new Discord.Client();
 
 // Command prefix
@@ -13,18 +16,31 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// Event is triggered when someone joins the server
+// Event is triggered when someone joins the channel
 client.on('guildMemberAdd', member => {
     const channel = member.guild.channels.find(
-        ch => ch.name === 'general');
+        ch => ch.name === 'member-log');
         
     if (!channel) return;
-    channel.send(`Welcome to the server, ${member}`);
+    channel.send(`Welcome to the server, ${member} (${member.user.username})`);
+});
+
+// Event triggered when someone leaves
+client.on('guildMemberRemove', member => {
+    const channel = member.guild.channels.find(
+        ch => ch.name === 'member-log');
+
+        if (!channel) return;
+        channel.send(`Goodbye,${member} (${member.user.username})`);
 });
 
 // Listens for any messages sent in the server
 client.on('message', message => {
     let msg = message.content.toUpperCase();
+    let author = message.author;
+
+    // Log messages to console if not this client
+    if (author != client.user) console.log(`[${moment().format('M/D/YYYY, h:mm:ss a')}] ${author.username}: '${message.content}'`);
 
     if (msg === prefix + 'PING') {
         message.channel.send('Pong!');
